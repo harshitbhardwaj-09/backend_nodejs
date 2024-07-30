88bdd5fa9af68200698547ccf4f9fd501ec1c91f
harshitbhardwaj-09 <harshitbhardwaj99999@gmail.com>
2024-07-30 05:35:10 +0530

    updated

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,11 +3,25 @@ import {ApiError} from "../utils/ApiError.js";
 import {uploadOnCloudinary} from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { User} from "../models/user.model.js"
+
+const generateAccessAndRefreshToken=async(userId)=>{
+    try{
+        const user=await User.findById(userId);
+        const accessToken=user.generateAccessToken();
+        const refreshToken=user.generateRefreshToken();
+        user.refreshToken=refreshToken;
+        user.save({ validateBeforeSave:false });
+        return {accessToken,refreshToken};
+    }
+    catch(error){
+        throw new ApiError(500,"something went wrong while generating refresh and access token")
+    }
+}
 const registerUser=asyncHandler( async (req,res)=>{
-    console.log("Request body:", req.body);
+    //console.log("Request body:", req.body);
 
     const { fullname,email,username,password}= req.body;
-    console.log("fullname:", fullname, "email:", email, "username:", username, "password:", password);
+    //console.log("fullname:", fullname, "email:", email, "username:", username, "password:", password);
     if(
         [fullname,email,username,password].some((field)=>
         field?.trim()==="")
@@ -20,14 +34,14 @@ const registerUser=asyncHandler( async (req,res)=>{
     if(existedUser){
         throw new ApiError(409,"user with username and email already exist");
     }
-    console.log("files recieved",req.files);
+    //console.log("files recieved",req.files);
     const avatarLocalPath=req.files?.avatar?.[0]?.path;
-    console.log("Avatar file path:", avatarLocalPath);
+   // console.log("Avatar file path:", avatarLocalPath);
     //const coverImageLocalPath=req.files?.coverImage[0].path;
     let coverImageLocalPath;
     if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length>0){
         coverImageLocalPath=req.files.coverImage[0].path;
-        console.log("Cover image file path:", coverImageLocalPath);
+       // console.log("Cover image file path:", coverImageLocalPath);
     }
     if(!avatarLocalPath){
         throw new ApiError(400,"Avatarlocalpath is required");
@@ -39,6 +53,7 @@ const registerUser=asyncHandler( async (req,res)=>{
     }
    const user=await  User.create({
         fullname,
+        email,
         avatar:avatar.url,
         coverImage:coverImage?.url||"",
         password,
@@ -54,5 +69,22 @@ const registerUser=asyncHandler( async (req,res)=>{
         new ApiResponse(200,createdUser,"User registered successfully")
     )
 })
-
+const loginUser=asyncHandler(async(req,res)=>{
+    const {email,username,password} =req.body;
+    if(!(username && email)){
+        throw new ApiError(404,"username or email is required");
+    }
+    const user=await User.findOne({
+        $or: [{username},{email}];
+    })
+    if(!user){
+        throw new ApiError(404,"user does not exist");
+    }
+    const isPasswordValid= await user.isPasswordCorrect(password);
+    if(!isPasswordValid){
+        throw new ApiError(401,"invalid user credentials");
+    }
+    const { accessToken, refreshToken }=await generateAccessAndRefreshToken(user._id);
+    const loggedInUser=await User.findById(user._id).select("-password -refreshToken")
+})
 export {registerUser}
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -11,19 +11,20 @@ cloudinary.config({
 const uploadOnCloudinary = async (localFilePath) => {
     
     try {
-        console.log('file path:',localFilePath);
+       // console.log('file path:',localFilePath);
         if (!localFilePath) return null;
         const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type: "auto"
         })
         // file has been uploaded successfull
-        console.log("file is uploaded on cloudinary ", response.url);
+        //console.log("file is uploaded on cloudinary ", response.url);
         //fs.unlinkSync(localFilePath)
         return response;
 
     } catch (error) {
+        console.log(error);
         fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
-        return null;
+        return error;
     }
 }
 
