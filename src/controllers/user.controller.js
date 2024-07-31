876c4b5d23b19e874add51457fe3022a5b0e8146
harshitbhardwaj-09 <harshitbhardwaj99999@gmail.com>
2024-07-31 16:36:00 +0530

    updated

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,10 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-import {ApiError} from "../utils/ApiError.js";
-import {uploadOnCloudinary} from "../utils/cloudinary.js";
-import { ApiResponse } from "../utils/ApiResponse.js";
+import {ApiError} from "../utils/ApiError.js"
 import { User} from "../models/user.model.js"
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import { ApiResponse } from "../utils/ApiResponse.js";
+//import jwt from "jsonwebtoken"
+//import mongoose from "mongoose";
 
 const generateAccessAndRefreshToken=async(userId)=>{
     try{
@@ -71,7 +73,7 @@ const registerUser=asyncHandler( async (req,res)=>{
 })
 const loginUser=asyncHandler(async(req,res)=>{
     const {email,username,password} =req.body;
-    if(!(username && email)){
+    if(!(username || email)){
         throw new ApiError(404,"username or email is required");
     }
     const user=await User.findOne({
